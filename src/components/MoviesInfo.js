03a094ae09b9cd794d4e9bf2b0e359e5f1b8e289
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-08-05 03:03:15 +0300

    lab

---
+++ src/components/MoviesInfo.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
+
+import { Row, Col } from "react-bootstrap";
+
+class MoviesInfo extends React.Component {
+  render() {
+    return (
+      <>
+        {this.props.newArr
+          ? this.props.newArr.map((item) => {
+              return (
+                <Row>
+                  <Col>
+                    <Card style={{ width: "18rem" }}>
+                      // <Card.Img variant="top" src={item.poster} />
+                      //{" "}
+                      <Card.Body>
+                        // <Card.Title>{item.title}</Card.Title>
+                        // <Card.Text>{item.overview}</Card.Text>
+                        //{" "}
+                      </Card.Body>
+                      //{" "}
+                      <ListGroup className="list-group-flush">
+                        // <ListGroupItem>{item.avgVotes}</ListGroupItem>
+                        // <ListGroupItem>{item.count}</ListGroupItem>
+                        // <ListGroupItem>{item.popularity}</ListGroupItem>
+                        // <ListGroupItem>{item.release}</ListGroupItem>
+                        //{" "}
+                      </ListGroup>
+                      //{" "}
+                    </Card>
+                  </Col>
+                </Row>
+              );
+            })
+          : null}
+      </>
+    );
+  }
+}
+export default MoviesInfo;
+
+{
+  /* <>
+                  <p>{item.title}</p>
+                  <p>{item.overview}</p>
+                  <p>{item.avgVotes}</p>
+                  <p>{item.poster}</p>
+                  <p>{item.popularity}</p>
+                  <p>{item.release}</p>
+                  <p>{item.count}</p>
+                </> */
+}
